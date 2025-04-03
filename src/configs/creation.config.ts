7c8623c0df10bf3e541f4a8aa645724c6f5df6a0
Roger H <roger@Rogers-MacBook-Pro.local>
2025-04-03 14:43:37 +0800

    Update top creations

--- src/configs/creation.config.ts
+++ src/configs/creation.config.ts
@@ -21,9 +21,34 @@ export const topCreations: TopCreation[] = [
     videoUrl: "http://34.227.168.212:8000/api/v1/tasks/4b070dff-a638-40a3-9f2d-540ed93e6783/files/output%2Foutput.mp4" // Replace with actual video URL
   },
   {
-    title: "Nice to meet you",
-    description: "Text to speech & Lip sync: Nice to meet you",
-    videoUrl: "http://34.227.168.212:8000/api/v1/tasks/2c6ed1bc-ff92-432a-b567-d97d3257c5f4/files/output%2Foutput.mp4" // Replace with actual video URL
+    title: "Raccoon",
+    description: "A cute raccoon in a soft, colorful storybook-style forest finds a shiny red apple under a big oak tree, eyes wide with joy. hand-painted picture book style",
+    videoUrl: "http://34.227.168.212:8000/api/v1/tasks/c1d6a35f-ef88-4a65-b687-be1fb9b344b5/files/output%2Foutput.mp4" // Replace with actual video URL
+  },
+  {
+    title: "raccoon and bunny",
+    description: "In a cozy, dreamy children’s book style, the raccoon and bunny sit under a big tree, happily sharing the apple, surrounded by falling autumn leaves. hand-painted picture book style",
+    videoUrl: "http://34.227.168.212:8000/api/v1/tasks/fef61bca-6dff-4148-a6d0-bc3ad354470f/files/output%2Foutput.mp4" // Replace with actual video URL
+  },
+  {
+      title: "Ads introduce boy",
+      description: "A handsome male with Indian feature is sitting at a clean desk. The overall vibe is warm and full of everyday life. he is speaking directly to the camera, with expressive facial gestures. While speaking, he slightly turns his head to make eye contact with the camera. Both hands rest on the table without holding anything.",
+      videoUrl: "http://34.227.168.212:8000/api/v1/tasks/6a7ae037-95ec-4124-a58a-6e0cfba8e69a/files/output%2Foutput.mp4" // Replace with actual video URL
+  },
+  {
+    title: "cartton",
+    description: "A beautiful Himalayan mountain village with small wooden houses and lush green hills. In the foreground, Binya, a young Indian girl with long black hair braided into two pigtails, wears a blue and white traditional dress. She stands happily looking at the valley, with a small leopard claw pendant hanging around her neck. The sky is bright and clear. traditional Indian animation style.",
+    videoUrl: "http://34.227.168.212:8000/api/v1/tasks/28fe22ea-74df-49f7-bf9a-89805c5505c0/files/output%2Foutput.png" // Replace with actual video URL
+  },
+  {
+      title: "indian girl",
+      description: "an india girl sitting in a temple",
+      videoUrl: "http://34.227.168.212:8000/api/v1/tasks/41747e81-ae02-43e4-9882-eee06b7b83d8/files/output%2Foutput.png" // Replace with actual video URL
+  },
+  {
+    title: "indian girl",
+    description: "a india girl",
+    videoUrl: "http://34.227.168.212:8000/api/v1/tasks/ec386f93-073e-44d5-aa40-1cb0c8a81a4b/files/output%2Foutput.png" // Replace with actual video URL
   },
   {
     title: "Lighthouse in a blizzard",
